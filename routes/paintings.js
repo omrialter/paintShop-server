cdffbe9e2995018d29b537d9db0385ef372c9df6
omri <omrialter3@gmail.com>
2025-01-24 19:07:51 +0200

    cloudinaryWorks

--- routes/paintings.js
+++ routes/paintings.js
@@ -2,6 +2,16 @@ const express = require("express");
 const router = express.Router();
 const { PaintingModel, validatePainting } = require("../models/paintingModel")
 const { auth } = require("../auth/auth.js");
+const cloudinary = require("cloudinary").v2;
+const { config } = require("../config/secret");
+
+
+
+cloudinary.config({
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.CLOUD_KEY,
+    api_secret: process.env.CLOUD_SECRET
+});
 
 router.get("/", async (req, res) => {
     res.json({ msg: "Paintings work" });
@@ -60,6 +70,17 @@ router.post("/", auth, async (req, res) => {
     }
     catch (err) {
         console.log(err);
+    }
+})
+
+router.post("/cloud", async (req, res) => {
+    try {
+        const dataUpload = await cloudinary.uploader.upload(req.body.image, { unique_filename: true })
+        res.json({ data: dataUpload });  // Respond with the uploaded image data
+    }
+    catch (err) {
+        console.log(err);
+        res.status(502).json({ err })
     }
 })
 
